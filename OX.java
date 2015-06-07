cdc0112fa2ac3afb74c82c4aec8c6084be0ae61a
oat9002 <oat9002@hotmail.com>
2015-06-07 21:42:02 +0700

    เพิ่ม player turn

--- OX.java
+++ OX.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;;
 
 public class OX {
 	
-	String[][] table = new String[5][5]; 
+	String[][] table = new String[5][5];
+	private Scanner input; 
 	
 	public void setup()
 	{
@@ -35,5 +37,32 @@ public class OX {
 			}
 			System.out.println();
 		}
+	}
+	
+	public void playerTurn() 
+	{
+		int  x,y = 0;
+		input = new Scanner(System.in);
+		while(true) {
+			System.out.print("Insert O position : ");
+			x = input.nextInt();
+			y = input.nextInt();
+			if(x <= 3 && y <= 3 && x > 0 && y > 0) {
+				break;
+			}
+		}
+		if(x == 1) {
+			x -= 1;
+		}
+		else if(x == 3) {
+			x += 1;
+		}
+		if(y == 1) {
+			y -= 1;
+		}
+		else if(y == 3) {
+			y += 1;
+		}
+		table[y][x] = "o";
 	}
 }
--- Run.java
+++ Run.java
@@ -1,10 +1,13 @@
 
-
 public class Run {
 	public static void main(String args[])
 	{
 		OX start = new OX();
 		start.setup();
 		start.printTable();
+		while(true) {
+			start.playerTurn();
+			start.printTable();
+		}
 	}
 }
